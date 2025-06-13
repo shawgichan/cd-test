d34b913e033c28ddb738d5d1edfc0d4c4a800a2e
Shawgi Hafiz <shawgichan@gmail.com>
2025-06-13 09:23:00 +0400

    test deploy

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	http.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "pong")
+		fmt.Fprintln(w, "pongneto")
 	})
 	http.ListenAndServe(":8000", nil)
 }
